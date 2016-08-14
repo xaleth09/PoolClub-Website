6ff492c1a9253758dd84a96f989ca82803547e7b
Roberto Garza <xaleth09@gmail.com>
2016-08-14 04:18:21 -0400

    cleaning up files

--- scripts/assign3.js
+++ scripts/assign3.js
@@ -1,15 +1,15 @@
 //if browser does not support the imageobject, exit.
 var slideimages = new Array() // create new array to preload images
 slideimages[0] = new Image() // create new instance of image object
-slideimages[0].src = "src/BUBC LOGO.png" // set image src property to image path, preloading image in the process
+slideimages[0].src = "../src/BUBC LOGO.png" // set image src property to image path, preloading image in the process
 slideimages[1] = new Image()
-slideimages[1].src = "src/cues.jpg"
+slideimages[1].src = "../src/cues.jpg"
 slideimages[2] = new Image()
-slideimages[2].src = "src/genMeeting.jpg"
+slideimages[2].src = "../src/genMeeting.jpg"
 slideimages[3] = new Image()
-slideimages[3].src = "src/splash.jpg"
+slideimages[3].src = "../src/splash.jpg"
 slideimages[4] = new Image()
-slideimages[4].src = "src/winterSux.jpg"
+slideimages[4].src = "../src/winterSux.jpg"
 
 //variable that will increment through the images
 var step=0
